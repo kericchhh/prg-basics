eb2a21ac6f27e5d9cb1d7d242d7bdc86ee5573c1
Karim Kerimbekov <karim.k2203@gmaill.com>
2024-10-13 14:20:18 +0300

    hw2

---
+++ 02-TypesAndVariables/5/2.py
@@ -0,0 +1,7 @@
+###
+# A program that prints your initials
+#
+name = 'George'
+surname = 'Lucas'
+print(name[0])
+print(surname[0])
---
+++ 02-TypesAndVariables/5/3.py
@@ -0,0 +1,7 @@
+###
+# A program that prints a university abbreviation
+#   
+university = "Krakow University of Economics"
+print(university[0])
+print(university[7])
+print(university[21])
